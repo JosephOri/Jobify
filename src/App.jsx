2860c7e99621965766576d76695895bd8943bf9f
Ori <ori100no@gmail.com>
2024-02-03 14:44:29 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import {
   Login,
   DashboardLayout,
   Error,
+  AddJob,
+  Stats,
+  Admin,
+  AllJobs,
+  Profile,
 } from './pages';
 
 const router = createBrowserRouter([
@@ -29,6 +34,26 @@ const router = createBrowserRouter([
       {
         path: 'dashboard',
         element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+          },
+          { path: 'stats', element: <Stats /> },
+          {
+            path: 'all-jobs',
+            element: <AllJobs />,
+          },
+
+          {
+            path: 'profile',
+            element: <Profile />,
+          },
+          {
+            path: 'admin',
+            element: <Admin />,
+          },
+        ],
       },
     ],
   },
